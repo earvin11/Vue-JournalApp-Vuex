d7d5b50f7904664be5e695f75251ce1eed20298c
Earvin <earvinsantiago.11@gmail.com>
2022-11-20 18:41:10 -0400

    Final section

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -5,5 +5,15 @@ const journalApi = axios.create({
     baseURL: 'https://vue-demos-d887a-default-rtdb.firebaseio.com'
 });
 
+journalApi.interceptors.request.use(( config ) => {
+
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+
+    return config
+
+})
+
 
 export default journalApi;
